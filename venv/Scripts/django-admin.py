d5833419542d79865901c2908d81bd5879578a6f
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-03-29 11:30:59 -0700

    venv stuff

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\silverstein\onedrive\documents\dojoassignments\projectweek\week1\ultimate_fan_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
