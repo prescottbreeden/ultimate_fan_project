a57eed8ff204456592bd23af9c061b1003d322a8
prescott breeden <prescott.breeden@gmail.com>
2018-05-28 19:08:08 -0700

    integrated sass structure into original project folder
    see "UF-sytle" repository for previous commits

--- apps/login/static/login/app.js
+++ apps/login/static/login/app.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-    console.log('testing-link');
-    var c = 0;
+    
     $(document).on('click', '#ready_btn', function() {
-        console.log('clicked ready');
-        
         $('#welcome_hero').stop().animate({left: -3000}, 'slow');
-
         setTimeout(function() {
-            $('.btn__form').addClass('shadow');
+            $('.btn__form').addClass('u-shadow');
         },500)
     })
 
     $(document).on('click', '#close_forms', function() {
-        console.log('clicked close');
-        $('.btn__form').removeClass('shadow');
+        $('.btn__form').removeClass('u-shadow');
         $('#welcome_hero').stop().animate({left: +0 }, 'slow');
-
     })
+
+
+
 })
